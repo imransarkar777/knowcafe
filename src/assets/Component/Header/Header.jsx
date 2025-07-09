9eaf14d0ab839933a308574de9630269910c2c0d
Imran25_1_test <imransarkar.dev@gmail.com>
2025-07-10 01:16:13 +0600

    Add `prop-types` dependency and update styles with new font
    Introduces `prop-types` to manage React component prop validation. Updates the header layout with improved styling and adds a profile image. Replaces custom CSS with Tailwind utility classes and adopts the "Exo 2" font for better design consistency.

--- src/assets/Component/Header/Header.jsx
+++ src/assets/Component/Header/Header.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Header = () => {
   return (
     <div>
-      <div>
+      <div className="flex justify-between items-center p-4 mx-4 border-b-2">
         <p className="bg-amber-600 text-amber-300 text-7xl">KnowCafe</p>
+        <img src="/images/profile.png" alt="" />
       </div>
     </div>
   );
