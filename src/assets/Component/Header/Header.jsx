3bfaa3fa585662ec672f7ae436338a8697eb8361
Imran25_1_test <imransarkar.dev@gmail.com>
2025-07-09 23:19:37 +0600

    Add dependencies and update application structure
    Introduces new dependencies (`i`, `pnpm`, `yarn`) to enhance
    package management and functionality. Updates the application
    structure by removing unused styles and placeholder content
    and integrating `Header` and `Blogs` components into the main
    application. Cleans up redundant code to improve maintainability.

---
+++ src/assets/Component/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <p className="bg-amber-600 text-amber-300 text-7xl">KnowCafe</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
